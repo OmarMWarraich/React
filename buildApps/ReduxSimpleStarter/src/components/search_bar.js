09b99625df79bcc3b9c22dedac0fb959f0ac7362
OmarMWarraich <earthling986@gmail.com>
2022-01-25 00:53:26 +0500

    Ajax Requests with React

--- buildApps/ReduxSimpleStarter/src/components/search_bar.js
+++ buildApps/ReduxSimpleStarter/src/components/search_bar.js
@@ -5,19 +5,23 @@ class SearchBar extends Component {
         super(props);
 
         this.state = { term: '' };
-    }
-
+    };
 
     render() {
         return ( 
-            <div>
-                value = {this.state.term};
-                <input onChange = {event => this.setState({term: event.target.value})} />
-
+            <div className="search-bar">
+                <input 
+                    value = {this.state.term} 
+                    onChange = {event => this.onInputChange(event.target.value)} 
+                />
         </div>
-        )
+        );
     }
-}
 
+    onInputChange(term) {
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
+    }
+};
 
 export default SearchBar;
