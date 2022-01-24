0240a6189e05e9b7215f98edb17123bcb9297e53
OmarMWarraich <earthling986@gmail.com>
2022-01-25 01:23:31 +0500

    Reorganized code

--- buildApps/ReduxSimpleStarter/src/index.js
+++ buildApps/ReduxSimpleStarter/src/index.js
@@ -1,51 +1,49 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-import VideoDetail from './components/video_detail';
-import VideoList from './components/video_list';
-
-
-const API_KEY = 'AIzaSyAQ6NCm5r5CP2iGIK9QltdL94M2WCxcICo';
+import _ from "lodash";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+const API_KEY = "AIzaSyDgAD2WkI4rwWMoIpYiRHWCVH7yg1MR3lw";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            videos: [],
-            selectedVideo: null 
-        };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-        this.videoSearch('surfboards');
-    }
+    this.videoSearch("surfboards");
+  }
 
-    videoSearch(term) {
-        YTSearch({ key: API_KEY, term: term }, videos => {
-          this.setState({
-            videos: videos,
-            selectedVideo: videos[0]
-          });
-        });
-      }
-    
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
 
-    render() {
-        const videoSearch = _.debounce(term => {
-            this.videoSearch(term);
-        }, 300);
+  render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
 
-        return (
-            <div>
-            <SearchBar onSearchTermChange={videoSearch}/>
-            <VideoDetail video={this.state.selectedVideo}/>
-            <VideoList 
-                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-                videos={this.state.videos}/>
-        </div>
-        );
-    }
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
 }
-    
-ReactDOM.render(<App />, document.querySelector('.container'));
+
+ReactDOM.render(<App />, document.querySelector(".container"));
